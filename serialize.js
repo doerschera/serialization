781db0e464296593c626ac185875ac3a6d0ce257
doerschera <doerschera@gmail.com>
2016-12-13 19:19:38 -0600

    html tags for unnested objects

--- serialize.js
+++ serialize.js
@@ -9,24 +9,42 @@ function serialize(data, outputFormat) {
 
   data.forEach(function(entry) {
     if(Array.isArray(entry)) {
-      entry.forEach(function(item) {
-        return console.log(typeof item);
-      })
+      itemIsArray(entry);
     } else if(typeof entry === 'object'){
-      console.log('Is object');
       itemIsObject(entry);
     }
   })
 
   function itemIsObject(item) {
+    var html = '<dl>';
     for(var key in item) {
       var childItem = item[key];
-      console.log('  '+typeof childItem);
-      if(typeof childItem === 'object') {
-        console.log('me!');
+      html+='\n<dt>'+key+'</dt>';
+      html+='\n<dd>'+childItem+'</dd>';
+      // console.log(html);
+      if(Array.isArray(childItem)) {
+        itemIsArray(childItem);
+      } else if(typeof childItem === 'object') {
         itemIsObject(childItem);
       }
     }
+    html += '</dd>';
+    return console.log(html);
+  }
+
+  function itemIsArray(item) {
+    var html = '<ul>';
+    item.forEach(function(item, i) {
+      // console.log(html);
+      html += '\n<li>'+item+'</li>';
+      if(Array.isArray(item)) {
+        return itemIsArray(item);
+      } else if(typeof item === 'object') {
+        itemIsObject(item);
+      }
+    })
+    html+= '\n</ul>'
+    return console.log(html);
   }
 }
 
