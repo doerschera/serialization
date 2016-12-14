f8e780d85da32d560c97a480d35ddbeb64988beb
doerschera <doerschera@gmail.com>
2016-12-13 18:39:27 -0600

    basic loop for element identification

--- serialize.js
+++ serialize.js
@@ -1,3 +1,24 @@
 var data = require('./testData.js');
 
-console.log(JSON.stringify(data));
+
+function serialize(data, outputFormat) {
+
+  if(outputFormat === 'JSON') {
+    return JSON.stringify(data);
+  }
+
+  data.forEach(function(entry) {
+    if(Array.isArray(entry)) {
+      entry.forEach(function(item) {
+        return console.log(typeof item);
+      })
+    } else if(typeof entry === 'object'){
+      console.log('Is object');
+      for(var key in entry) {
+        console.log(typeof entry[key]);
+      }
+    }
+  })
+}
+
+serialize(data, 'HTML');
